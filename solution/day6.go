3da6ca5d99b20084dbb0d473bf559765f40e755f
Jake Barszcz <jfbarszcz@gmail.com>
2022-12-06 10:03:05 -0500

    use a single count map, make it even more linear-er

--- solution/day6.go
+++ solution/day6.go
@@ -12,7 +12,7 @@ func init() {
 
 type Day6 struct{}
 
-func (d *Day6) solve(input []byte, distinctCount int) string {
+func (d *Day6) solveWithSets(input []byte, distinctCount int) string {
 	for i := range input {
 		if i < distinctCount {
 			continue
@@ -25,10 +25,33 @@ func (d *Day6) solve(input []byte, distinctCount int) string {
 	panic("unreachable")
 }
 
+func (d *Day6) solveWithCountMap(input []byte, distinctCount int) string {
+	countMap := make(map[byte]int)
+	for i, char := range input {
+		_, ok := countMap[char]
+		if !ok {
+			countMap[char] = 0
+		}
+		countMap[char]++
+		if i < distinctCount {
+			continue
+		}
+		outChar := input[i-distinctCount]
+		countMap[outChar]--
+		if countMap[outChar] == 0 {
+			delete(countMap, outChar)
+		}
+		if len(countMap) == distinctCount {
+			return strconv.Itoa(i + 1)
+		}
+	}
+	panic("unreachable")
+}
+
 func (d *Day6) Part1(input []byte) string {
-	return d.solve(input, 4)
+	return d.solveWithCountMap(input, 4)
 }
 
 func (d *Day6) Part2(input []byte) string {
-	return d.solve(input, 14)
+	return d.solveWithCountMap(input, 14)
 }
