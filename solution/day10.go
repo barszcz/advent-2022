3e818a6069ee3c95650f8ae7e522aaeb7533252a
Jake Barszcz <jfbarszcz@gmail.com>
2022-12-10 10:06:57 -0500

    day 10 part 2

--- solution/day10.go
+++ solution/day10.go
@@ -3,9 +3,11 @@ package solution
 import (
 	"bufio"
 	"bytes"
-	"fmt"
+	"math"
 	"strconv"
 	"strings"
+
+	"github.com/barszcz/advent-2022/internal/util"
 )
 
 func init() {
@@ -33,16 +35,49 @@ func (d *Day10) Part1(input []byte) string {
 	signalSum := 0
 	for i := 20; i < len(xValues); i += 40 {
 		signal := xValues[i-1] * i
-		fmt.Printf("signal: %d * %d = %d\n", i, xValues[i-1], signal)
 		signalSum += signal
 	}
-	for i := 215; i < 225; i++ {
-		fmt.Printf("%d is %d\n", i, xValues[i-1])
-	}
-	// fmt.Println(xValues[215:225])
 	return strconv.Itoa(signalSum)
 }
 
 func (d *Day10) Part2(input []byte) string {
-	return ""
+	lines := bufio.NewScanner(bytes.NewReader(input))
+
+	x := 1
+	xValues := []int{1}
+	for lines.Scan() {
+		line := lines.Text()
+		xValues = append(xValues, x)
+		if line == "noop" {
+			continue
+		}
+		incr := Must(strconv.Atoi(strings.TrimPrefix(line, "addx ")))
+		x += incr
+		xValues = append(xValues, x)
+	}
+
+	pixels := make([]bool, 0, 240)
+	for i := 0; i < 240; i++ {
+		pixelVal := math.Abs(float64(((i % 40) - xValues[i]))) <= 1
+		pixels = append(pixels, pixelVal)
+	}
+
+	pixelLines := util.ChunkSlice(pixels, 40)
+
+	var builder strings.Builder
+
+	builder.WriteString("\n")
+	for _, pixelLine := range pixelLines {
+		for _, pixel := range pixelLine {
+			if pixel {
+				builder.WriteString("#")
+			} else {
+				builder.WriteString(".")
+			}
+		}
+		builder.WriteString("\n")
+	}
+
+	return builder.String()
+
 }
